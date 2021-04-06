c6b8b64382432a25b3ac4e6943fb34281aa35a0a
Minerkow <melnikov.ignat@phystech.edu>
2021-04-06 23:54:32 +0200

    update parse arguments

--- ParseInput.py
+++ ParseInput.py
@@ -1,5 +1,6 @@
 import argparse
 import enum
+import sys
 
 
 class ConnectionParameters(enum.Enum):
@@ -15,31 +16,41 @@ class RoleParameters(enum.Enum):
 class SessionParameters:
     port = int
     ip = str
-    connectionParameters = ConnectionParameters
+    connection_parameters = ConnectionParameters
     role = RoleParameters
+    output_logs = None
 
-    def __init__(self, port, ip, connection_parameters, role):
+    def __init__(self, port, ip, connection_parameters, role, output_logs):
         self.port = port
         self.ip = ip
         self.connectionParameters = connection_parameters
         self.role = role
+        self.output_logs = output_logs
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser("Connection Argument Parser")
-    group = parser.add_mutually_exclusive_group(required=False)
+    group_connection_flags = parser.add_mutually_exclusive_group(required=False)
+    group_logs_flags = parser.add_mutually_exclusive_group(required=True)
 
     parser.add_argument("host", help="Server ip address", type=str)
     parser.add_argument("port", help="Port for connection", type=int)
     parser.add_argument("-s", help="Start Server", action="store_true")
     parser.set_defaults(s=False)
 
-    group.add_argument("-t", help="TCP mode", action="store_true")
-    group.add_argument("-u", help="UDP mode", action="store_true")
-    group.set_defaults(t=True, u=False)
+    group_connection_flags.add_argument("-t", help="TCP mode", action="store_true")
+    group_connection_flags.add_argument("-u", help="UDP mode", action="store_true")
+    group_connection_flags.set_defaults(t=True, u=False)
+
+    group_logs_flags.add_argument("-o", help="Output of logs to stdout",
+                                  action="store_true")
+    group_logs_flags.add_argument("-f", help="output of logs to file",
+                                  type=str)
 
     args = parser.parse_args()
 
     type_connection = ConnectionParameters.UDP if args.u else ConnectionParameters.TCP
     role = RoleParameters.SERVER if args.s else RoleParameters.CLIENT
-    return SessionParameters(args.host, args.port, type_connection, role)
+    output_logs = sys.stdout if args.o else open(args.f, "w")
+
+    return SessionParameters(args.host, args.port, type_connection, role, output_logs)

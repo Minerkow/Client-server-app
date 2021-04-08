0ddc11166df5047b485df10b4183bde9bf51927d
Minerkow <melnikov.ignat@phystech.edu>
2021-04-08 23:31:50 +0200

    Added some error handling, customization of abstract methods and a few more fixes

--- Client.py
+++ Client.py
@@ -1,8 +1,11 @@
 import socket
 import logging
 
+from abc import ABC, abstractmethod
 
-class Client:
+
+class Client(ABC):
+    """Base class for the client"""
 
     BUF_SIZE = 2048
 
@@ -14,23 +17,39 @@ class Client:
         self.server_ip = server_ip
         self.server_port = server_port
 
+    @abstractmethod
+    def start(self):
+        """Basic method for starting client"""
+        pass
+
 
 class UDPClient(Client):
+    TIME_OUT = 2
+
     def __init__(self, server_ip, server_port):
         super().__init__(server_ip, server_port)
 
     def start(self):
+        str_server_addr = self.server_ip + ':' + str(self.server_port)
+
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         logging.info(' UDPClient: Client socket open')
 
         hello_msg = bytes('Hello', 'UTF-8')
         self.client_socket.sendto(hello_msg, (self.server_ip, self.server_port))
-        logging.info(' UDPClient: Msg sent to server')
+        logging.info(' UDPClient: Sending message to server '
+                     'with address: %s', str_server_addr)
 
-        recv_message, server_address = self.client_socket.recvfrom(Client.BUF_SIZE)
-        logging.info(' UDPClient: Msg receive from server')
+        try:
+            self.client_socket.settimeout(UDPClient.TIME_OUT)
+            recv_message, server_address = self.client_socket.recvfrom(Client.BUF_SIZE)
+            logging.info(' UDPClient: Received reply message from the server '
+                         'with address: %s', str_server_addr)
+            print(str(recv_message, 'UTF-8'))
+        except socket.timeout:
+            logging.error(' UDPClient: No response from the server has been '
+                          'received within the allotted time')
 
-        print(str(recv_message, 'UTF-8'))
         self.client_socket.close()
         logging.info(' UDPClient: Client socket closed')
 
@@ -40,25 +59,28 @@ class TCPClient(Client):
         super().__init__(server_ip, server_port)
 
     def start(self):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.server_ip, self.server_port))
         str_server_addr = self.server_ip + ':' + str(self.server_port)
-        logging.info(' TCPClient: Connect with server with ip: ' +
+
+        try:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((self.server_ip, self.server_port))
+        except socket.error as err:
+            logging.exception(err)
+            self.client_socket.close()
+            return
+
+        logging.info(' TCPClient: Connect to server with address: %s',
                      str_server_addr)
 
         hello_msg = bytes('Hello', 'UTF-8')
         self.client_socket.send(hello_msg)
-        logging.info(' TCPClient: Send msg to server with ip: ' +
-                     str_server_addr)
+        logging.info(' TCPClient: Sending a message to server '
+                     'with address: %s', str_server_addr)
 
         recv_msg = self.client_socket.recv(Client.BUF_SIZE)
-        logging.info(' TCPClient: Receive msg from server with ip: ' +
-                     str_server_addr)
+        logging.info(' TCPClient: Received a reply message from the server '
+                     'with address: %s', str_server_addr)
         print(str(recv_msg, 'UTF-8'))
 
         self.client_socket.close()
         logging.info(' TCPClient: Client socket closed')
-
-
-
-
--- Server.py
+++ Server.py
@@ -1,8 +1,11 @@
 import logging
 import socket
 
+from abc import ABC, abstractmethod
 
-class Server:
+
+class Server(ABC):
+    """Base class for the server"""
     BUF_SIZE = 2048
 
     server_port = int
@@ -10,6 +13,11 @@ class Server:
 
     def __init__(self, server_port):
         self.server_port = server_port
+
+    @abstractmethod
+    def start(self):
+        """Basic method for starting the server"""
+        pass
 
 
 class UDPServer(Server):
@@ -20,18 +28,23 @@ class UDPServer(Server):
     def start(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket.bind(('', self.server_port))
-        logging.info(' UDPServer: Server socket open with port ' + str(self.server_port))
+        logging.info(' UDPServer: Server socket open with port %s', str(self.server_port))
 
         while True:
             recv_message, client_address = self.server_socket.recvfrom(Server.BUF_SIZE)
-            logging.info(' UDPServer: Receive msg from client')
+            str_client_address = client_address[0] + ':' + str(client_address[1])
+            logging.info(' UDPServer: Receive message from client with address: %s',
+                         str_client_address)
 
-            send_msg = bytes(client_address[0] + ':' + str(client_address[1]), 'UTF-8')
-            self.server_socket.sendto(send_msg, client_address)
-            logging.info(' UDPServer: Send msg to client')
+            self.server_socket.sendto(bytes(str_client_address, 'UTF-8'),
+                                      client_address)
+            logging.info(' UDPServer: Send message to client with address: %s',
+                         str_client_address)
 
 
 class TCPServer(Server):
+    TIME_OUT = 2
+
     def __init__(self, server_port):
         super().__init__(server_port)
 
@@ -39,23 +52,32 @@ class TCPServer(Server):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(('', self.server_port))
         self.server_socket.listen(1)
-        logging.info(' TCPServer: Server socket open with port' +
-                     ' ' +  str(self.server_port))
+        logging.info(' TCPServer: Server socket open with port: %s',
+                     str(self.server_port))
 
         while True:
             connection_socket, client_addr = self.server_socket.accept()
             str_client_addr = client_addr[0] + ':' + str(client_addr[1])
-            logging.info(' TCPServer: Accept connection with ip: ' +
+            logging.info(' TCPServer: Accept connection with address: %s',
                          str_client_addr)
 
-            recv_msg = connection_socket.recv(Server.BUF_SIZE)
-            logging.info(' TCPServer: Receive msg from client with ip: ' +
-                         str_client_addr)
+            try:
+                connection_socket.settimeout(2)
+                connection_socket.recv(Server.BUF_SIZE)
+                logging.info(' TCPServer: Receive msg from client '
+                             'with address: %s', str_client_addr)
+            except socket.timeout:
+                logging.error(' TCPServer: Message from client not received '
+                              'with address %s, disconnection', str_client_addr)
+                connection_socket.close()
+                logging.info(' TCPServer: Close connection with address: %s',
+                             str_client_addr)
+                continue
 
             connection_socket.send(bytes(str_client_addr, 'UTF-8'))
-            logging.info(' TCPServer: Send msg to client with ip: ' +
+            logging.info(' TCPServer: Send msg to client with address: %s',
                          str_client_addr)
 
             connection_socket.close()
-            logging.info(' TCPServer: Close connection with ip: ' +
+            logging.info(' TCPServer: Close connection with address: %s',
                          str_client_addr)
